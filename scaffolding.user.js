8a98907ab4c40cc0774d8dbd48de9e8afe26ff91
jstecher <jlstecher@gmail.com>
2021-12-12 21:47:00 +0100

    char list: add graphical view

--- scaffolding.user.js
+++ scaffolding.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        AnneTrue's Nexus Tweaks Scaffolding
-// @version     1.5.0
+// @version     1.6.1
 // @description Scaffolding and API for nexus-tweaks
 // @namespace   https://github.com/AnneTrue/
 // @author      Anne True
