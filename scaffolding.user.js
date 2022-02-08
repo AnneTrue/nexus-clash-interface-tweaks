29b6d757f704fa1f3de21b36aa15b623df63a5d9
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-02-09 00:34:18 +0100

    Bump prev-version

--- scaffolding.user.js
+++ scaffolding.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        AnneTrue's Nexus Tweaks Scaffolding
-// @version     999.prev.2
+// @version     999.prev.3
 // @description Scaffolding and API for nexus-tweaks
 // @namespace   https://github.com/AnneTrue/
 // @author      Anne True
