e3ebaceb5d74a8a634a67829f2c8e57a927d1089
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-02-08 23:04:29 +0100

    bugfix Didn't work if you had the ability to summon pets. Didn't realize because I was testing on a pet-capped elementalist.

--- nexus-tweaks.user.js
+++ nexus-tweaks.user.js
@@ -1160,9 +1160,14 @@ promiseList.push((async () => {
     if (!petTableTable) {
       return;
     }
-    // Pets are on a table inside the pet table
-    const petTable = petTableTable.querySelector('tbody').querySelector('tbody');
-    const setAllRow = petTable.lastElementChild; // Last row is the set-all button
+
+    // Pets are on an unnamed table inside the pet table
+    // There may or may not be another table of class 'summonsButtons' (which you don't get if you're at pet cap)
+    const petTable = petTableTable.querySelector('tbody').querySelector('table:not(.summonsButtons)>tbody');
+    // With two title rows and a tail row, at least 4 rows are needed if there's any pet
+    if (petTable.children.length < 4) return;
+    // Last row is the set-all button
+    const setAllRow = petTable.lastElementChild;
 
     // Pets start on the third row
     for (const petRow of Array.from(petTable.children).slice(2, -1)) {
