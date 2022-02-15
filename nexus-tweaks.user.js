bbc1c23bf497e581161435872ef1f3d196a6ce4f
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-02-15 12:32:16 +0100

    style fixes
    FM -> fossilMonstrosity
    commented on DOM modifications

--- nexus-tweaks.user.js
+++ nexus-tweaks.user.js
@@ -1161,19 +1161,21 @@ promiseList.push((async () => {
 
     // Pets summon buttons
     const petSummons = petTableTable.querySelector('tbody').querySelector('table.summonsButtons>tbody');
-    const FM = petSummons.querySelector('input[value="Fossil Monstrosity"]');
+    const fossilMonstrosity = petSummons.querySelector('input[value="Fossil Monstrosity"]');
     const necro = petSummons.querySelector('input[value="Necrophage"]');
 
-    if (FM) {
+    if (fossilMonstrosity) {
       // This is how they currently display by default
-      // FM.nextSibling.value = 'Fossil Monstrosity (3 Skeletons) '
+      // fossilMonstrosity.nextSibling.value = 'Fossil Monstrosity (3 Skeletons) '
       if (petSummons.children.length < 3) petSummons.appendChild(document.createElement('tr'));
-      petSummons.children[2].insertBefore(FM.parentNode.parentNode, petSummons.children[2].firstChild);
+      // Second row on the summoning panel, below Skeletons et al
+      petSummons.children[2].insertBefore(fossilMonstrosity.parentNode.parentNode, petSummons.children[2].firstChild);
     }
     if (necro) {
       // This is how they currently display by default
       // necro.nextSibling.value = 'Necrophage (3 Zombies/Ghouls) '
       if (petSummons.children.length < 3) petSummons.appendChild(document.createElement('tr'));
+      // Second row on the summoning panel, below Skeletons et al
       petSummons.children[2].appendChild(necro.parentNode.parentNode);
     }
   }
