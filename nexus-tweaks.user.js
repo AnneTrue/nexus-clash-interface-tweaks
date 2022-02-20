cff95d708333b0c4d5b49f0940091814bd52eb36
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-02-20 19:59:57 +0100

    Module: Default Set-All Pet Stance A very convenient tweak for petmasters.

--- nexus-tweaks.user.js
+++ nexus-tweaks.user.js
@@ -1416,6 +1416,47 @@ promiseList.push((async () => {
 
 
 //##############################################################################
+promiseList.push((async () => {
+  const mod = await nexusTweaks.registerModule(
+    'defaultSetAllPetStance',
+    'Default Set-All Pet Stance',
+    'local',
+    'Allows the user to set the default stance for Set All pets, instead of defaulting to "Passive". Doesn\'t affect default summoning stance.',
+  );
+
+  const defaultStance = async () => {
+    const petTable = document.querySelector('.petTable');
+    if (!petTable) {
+      mod.debug('No pet pane found.');
+      return;
+    }
+    const defaultStance = await mod.getSetting('default-stance');
+    petTable.querySelector('form[name="petstance"] select[name="stance"]').value = defaultStance;
+  }
+
+  await mod.registerSetting(
+    'select',
+    'default-stance',
+    'Default Stance',
+    'Choose the default pet stance',
+    [
+      {value: 'Passive', text: 'Passive'},
+      {value: 'Defensive', text: 'Defensive'},
+      {value: 'Aggressive to Hostiles', text: 'Hostiles'},
+      {value: 'Aggressive to Non-Allied', text: 'Non-Allied'},
+      {value: 'Aggressive to Non-Faction', text: 'Non-Faction'},
+      {value: 'Aggressive', text: 'All'},
+    ]
+  );
+
+  await mod.registerMethod(
+    'async',
+    defaultStance
+  );
+})());
+
+
+//##############################################################################
 // Must be last executed step, as this unlocks nexusTweaks to run
 (async () => {
   nexusTweaks.addGlobalStyle(await GM.getResourceUrl('nexusTweaksCSS'));
