2e3e05b04883a3e8df86a8ccec9d6ca0507366ee
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-02-22 12:35:31 +0100

    Version bump

--- nexus-tweaks.user.js
+++ nexus-tweaks.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        AnneTrue's Nexus Tweaks
-// @version     999.prev.23
+// @version     999.prev.24
 // @description Tweaks for Nexus Clash's UI
 // @namespace   https://github.com/AnneTrue/
 // @author      Anne True
