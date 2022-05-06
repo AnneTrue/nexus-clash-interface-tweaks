ea5c4c4d3577d9321184886ba3a3cfdc79925ea9
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-05-06 13:35:52 +0200

    Bugfix: API

--- nexus-tweaks.user.js
+++ nexus-tweaks.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        AnneTrue's Nexus Tweaks
-// @version     999.prev.52.3
+// @version     999.prev.52.4
 // @description Tweaks for Nexus Clash's UI
 // @namespace   https://github.com/AnneTrue/
 // @author      Anne True
--- scaffolding.js
+++ scaffolding.js
@@ -6,7 +6,7 @@ function NexusTweaksScaffolding(scriptId, scriptName, scriptLink, scriptVersion)
   // Given how GM does apparently ignore the metadata block on @require scripts, it could possibly be removed
   // Leaving it here for backwards-compatibility, in case any scripts need it
   this.version = `${GM.info.script.version}`;
-  this.APIversion = '999.api.24.1';
+  this.APIversion = '999.api.24.2';
   this.APIname = 'Nexus Tweaks API & Scaffolding';
   this.APIhomepage = 'https://github.com/Argavyon/nexus-clash-interface-tweaks/tree/preview';
   // logs to console; can disable if you want
@@ -286,14 +286,14 @@ function NexusTweaksScaffolding(scriptId, scriptName, scriptLink, scriptVersion)
       const paneClosed = panetitle.classList.contains('paneclosed');
       label_inp3.src = paneClosed ? 'images/g/inf/open.gif' : 'images/g/inf/close.gif';
       label_inp3.alt = paneClosed ? '+' : '-';
-      if (paneId) this.setSetting(closedSettingName, paneClosed);
+      if (paneId) mod.setSetting(closedSettingName, paneClosed);
     }
 
     const panecontent = document.querySelector('#main-left').insertBefore(document.createElement('div'), nextPane);
     panecontent.className = 'panecontent';
     return {title: panetitle, content: panecontent};
     
-    this.getSetting(closedSettingName).then(paneClosed => panetitle.onclick());
+    this.getSetting(closedSettingName).then(paneClosed => if (paneClosed) panetitle.click());
   }
 
 
