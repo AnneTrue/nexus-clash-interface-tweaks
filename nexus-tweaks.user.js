acbd06ca96c586581ee4b72637eba70a016cdcf5
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-05-16 23:41:55 +0200

    Module: Log Saver Enables easier saving of character logs

--- nexus-tweaks.user.js
+++ nexus-tweaks.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        AnneTrue's Nexus Tweaks
-// @version     999.prev.54.6
+// @version     999.prev.55
 // @description Tweaks for Nexus Clash's UI
 // @namespace   https://github.com/AnneTrue/
 // @author      Anne True
@@ -3819,6 +3819,68 @@ promiseList.push((async () => {
     await mod.registerMethod(
         'async',
         inPain
+    );
+})());
+
+
+//##############################################################################
+promiseList.push((async () => {
+    const mod = await argavyonExTweaks.registerModule(
+        'logSaver',
+        'Message Log Saver',
+        'global',
+        'Enables saving NC message log into a text file with a single press of a button.\n' +
+        'You\'ll need to reload before seeing the dowload button.',
+    );
+
+    const download = (filename, text) => {
+        const element = document.createElement('a');
+        element.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(text);
+        element.download = filename;
+        element.style.display = 'none';
+
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    }
+
+    const charLogsRequest = (charId, callback) => {
+        GM.xmlHttpRequest({
+            method: 'POST',
+            url: `https://www.nexusclash.com/clash.php?op=character&id=${charId}`,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            data: 'displaymessages=Display',
+            onload: callback
+        });
+    }
+
+    const logSaver = () => {
+        'use strict';
+        if (!mod.API.inGame) return;
+        const charID = mod.API.charinfo.id;
+        document.querySelector('#nexus-tweaks-settings-button').addEventListener('click', function() {
+            const dwldButton = document.querySelector('#nexus-tweaks-setting-logSaver')
+            .appendChild(document.createElement('tr'))
+            .appendChild(document.createElement('td'))
+            .appendChild(document.createElement('input'));
+            dwldButton.type = 'button';
+            dwldButton.value = 'Download Logs';
+            dwldButton.style.color = 'black';
+            dwldButton.onclick = function () {
+                charLogsRequest(charID, async function(response) {
+                    var parser = new DOMParser();
+                    var htmlDoc = parser.parseFromString(response.responseText, 'text/html');
+                    const logDiv = htmlDoc.querySelector('div#Messages');
+                    const logs = logDiv.textContent;
+                    download('logs.txt', logs);
+                });
+            };
+        });
+    }
+
+    await mod.registerMethod(
+        'sync',
+        logSaver
     );
 })());
 
