8097ad814c725057361bbbecc153e2deca606051
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-06-12 22:32:35 +0200

    Sync scaffolding version

--- scaffolding.js
+++ scaffolding.js
@@ -6,7 +6,7 @@ function NexusTweaksScaffolding(scriptId, scriptName, scriptLink, scriptVersion)
   // Given how GM does apparently ignore the metadata block on @require scripts, it could possibly be removed
   // Leaving it here for backwards-compatibility, in case any scripts need it
   this.version = `${GM.info.script.version}`;
-  this.APIversion = '999.api.25.2';
+  this.APIversion = '2.0.0';
   this.APIname = 'Nexus Tweaks API & Scaffolding';
   this.APIhomepage = 'https://github.com/Argavyon/nexus-clash-interface-tweaks/tree/preview';
   // logs to console; can disable if you want
