5fe376d1fa23de7d55287ca8e0a3994b8a942a85
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-06-12 18:48:19 +0200

    Split Potato Stance from the rest of class-specific tweaks

--- modules/classSpecificTweaks.js
+++ modules/classSpecificTweaks.js
@@ -65,31 +65,6 @@ const classSpecificTweaks = {
                 name: 'Lich - Pet Summon fix',
                 desc: 'Fixes the UI for summoning lich pets. Should make the pet list more eye-friendly.',
             },
-            'petmaster-potatoStance': {
-                callback: () => {
-                    const petTableTable = document.querySelector('.petTable');
-                    if (!petTableTable) {
-                        mod.debug('No pet table found.');
-                        return;
-                    }
-
-                    // Pets are on an unnamed table inside the pet table
-                    // There may or may not be another table of class 'summonsButtons' (which you don't get if you're at pet cap)
-                    const petTable = petTableTable.querySelector('tbody').querySelector('table:not(.summonsButtons)>tbody');
-                    // With two title rows and a tail row, at least 4 rows are needed if there's any pet
-                    if (petTable.children.length < 4) return;
-                    // Last row is the set-all button
-                    const setAllRow = petTable.lastElementChild;
-
-                    // Pets start on the third row
-                    for (const petRow of Array.from(petTable.children).slice(2, -1)) {
-                        petRow.querySelector('select[name="stance"] option[value="Passive"]').textContent = 'Potato';
-                    }
-                    setAllRow.querySelector('select[name="stance"] option[value="Passive"]').textContent = 'Potato';
-                },
-                name: 'Petmaster - Potato Stance',
-                desc: 'Replaces the Passive pet stance by a functionally-equivalent Potato stance. It\'s pretty useless. Absolutely useless.',
-            }
         };
 
         const mergeAlert = () => {
