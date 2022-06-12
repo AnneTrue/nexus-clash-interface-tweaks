c49183dba306623cb34b4789d7897e43a46daf55
Argavyon <75849479+Argavyon@users.noreply.github.com>
2022-06-12 19:22:25 +0200

    Modules display whether they're global on the settings tab

--- scaffolding.js
+++ scaffolding.js
@@ -6,7 +6,7 @@ function NexusTweaksScaffolding(scriptId, scriptName, scriptLink, scriptVersion)
   // Given how GM does apparently ignore the metadata block on @require scripts, it could possibly be removed
   // Leaving it here for backwards-compatibility, in case any scripts need it
   this.version = `${GM.info.script.version}`;
-  this.APIversion = '999.api.25';
+  this.APIversion = '999.api.25.1';
   this.APIname = 'Nexus Tweaks API & Scaffolding';
   this.APIhomepage = 'https://github.com/Argavyon/nexus-clash-interface-tweaks/tree/preview';
   // logs to console; can disable if you want
@@ -654,7 +654,7 @@ function NexusTweaksModule(API, id, name, localType, description) {
 
   this.getModuleEnableElement = async () => {
     const moduleEnableSetting = new NexusTweaksSetting(
-      API, 'checkbox', 'module_enabled', this.name, this.description, null
+      API, 'checkbox', 'module_enabled', (this.localType === 'global' ? '[G] ' : '') + this.name, this.description, null
     );
     return await moduleEnableSetting.getSettingsRowElement(this);
   }
